bd85c391ce1e8e41c1364ee6f564c3e94ccb1a3e
tvikto <tothkviktor@gmail.com>
2021-03-24 18:33:15 +0100

    2.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ function butFunction()
     let neve=prompt("Kérlek add meg a neved:");
     document.getElementById("first").innerHTML=("Üdvözöllek "+neve);
 }
+function picFunction1()
+{
+    document.getElementById("pic").style.width="500px";
+}
+function picFunction2()
+{
+    document.getElementById("pic").style.width="250px";
+}
