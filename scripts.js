087c141b184b3b1ef86da25d4e01903f6a5a3c63
tvikto <tothkviktor@gmail.com>
2021-03-24 18:37:42 +0100

    4.

--- scripts.js
+++ scripts.js
@@ -5,13 +5,17 @@ function butFunction()
 }
 function picFunction1()
 {
-    document.getElementById("pic").style.width="500px";
+    document.getElementById("pic").style.width="300px";
 }
 function picFunction2()
 {
-    document.getElementById("pic").style.width="250px";
+    document.getElementById("pic").style.width="150px";
 }
 function pageFunction3()
 {
     console.log("Az oldal betöltött!")
 }
+const text= document.getElementById("Lorem");
+    text.style.color = "blue";
+    text.style.textAlign = "justify";
+    text.style.width = "400px";
